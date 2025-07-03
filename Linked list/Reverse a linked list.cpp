93b5e29631eb1c230e95992541500c2751c7dc87
NayanB21 <nayanbalpande2116@gmail.com>
2025-07-03 15:26:43 +0530

    error fixing step

--- Linked list/Reverse a linked list.cpp
+++ Linked list/Reverse a linked list.cpp
@@ -1,34 +1,34 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
-class Solution {
+#include <bits/stdc++.h>
+using namespace std;
+
+// Definition for singly-linked list.
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution
+{
 public:
-    ListNode* reverseList(ListNode* head) {
-            
-            ListNode* h1=head;
-            if(h1==NULL||h1->next==NULL)return head;
-            ListNode* h2=head->next;
-            h1->next=NULL;
-            while(h2!=NULL){
-                ListNode* h3=h2->next;
-                h2->next=h1;
-                h1=h2;
-                h2=h3;
+    ListNode *reverseList(ListNode *head)
+    {
 
-
-            }
-            return h1;
-
-
-
-
+        ListNode *h1 = head;
+        if (h1 == NULL || h1->next == NULL)
+            return head;
+        ListNode *h2 = head->next;
+        h1->next = NULL;
+        while (h2 != NULL)
+        {
+            ListNode *h3 = h2->next;
+            h2->next = h1;
+            h1 = h2;
+            h2 = h3;
+        }
+        return h1;
     }
-       
 };
